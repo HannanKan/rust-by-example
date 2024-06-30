1bf741404f5f0013a3586587044dc8005c0eba12
hannankan <hannankan@foxmail.com>
2024-06-30 18:34:42 +0800

    完成18章 错误处理

---
+++ src/bin/18_2_1_unpacking_option.rs
@@ -0,0 +1,49 @@
+struct Person {
+    job: Option<Job>,
+}
+
+#[derive(Clone, Copy)]
+struct Job {
+    phone_number: Option<PhoneNumber>,
+}
+
+#[derive(Clone, Copy)]
+struct PhoneNumber {
+    area_code: Option<u8>,
+    number: u32,
+}
+
+impl Person {
+
+    // 获取此人的工作电话号码的区号（如果存在的话）。
+    fn work_phone_area_code(&self) -> Option<u8> {
+        // 没有`？`运算符的话，这将需要很多的嵌套的 `match` 语句。
+        // 这将需要更多代码——尝试自己编写一下，看看哪个更容易。
+        // self.job?.phone_number?.area_code
+
+        // quiz
+        match self.job {
+            Some(job) => match job.phone_number {
+                Some(phone_number) => match phone_number.area_code {
+                    Some(a_c) => Some(a_c),
+                    None => None,
+                }
+                None => None,
+            }
+            None => None,
+        }
+    }
+}
+
+fn main() {
+    let p = Person {
+        job: Some(Job {
+            phone_number: Some(PhoneNumber {
+                area_code: Some(61),
+                number: 439222222,
+            }),
+        }),
+    };
+
+    assert_eq!(p.work_phone_area_code(), Some(61));
+}
